f7b5623fdcee31a316c1915bb119baf4b36f80e8
Shalmali Padgaokar <shalmalidalvi@hotmail.com>
2016-05-13 14:14:52 -0400

    flags added partial CSS done

--- src/js/main.js
+++ src/js/main.js
@@ -6,6 +6,7 @@ let drop = $('.drop');
 
 ddtitle.on('click',function(event){
   event.preventDefault();
+  //pageData.text('');
   console.log('clicked title');
   var pageData = dropDown();
   drop.append(pageData);
@@ -14,31 +15,32 @@ ddtitle.on('click',function(event){
 
 arrow.on('click',function(event){
   event.preventDefault();
-  console.log('clicked arrow');
+  //pageData.text('');
+  console.log('clicked title');
+  var pageData = dropDown();
+  drop.append(pageData);
 
 });
 
 
 function dropDown(){
+  drop.text('');
   return`
-    <div class='each'>
-      <img src="">  
-      <h6 class='lang'>Romania - Romana</h6>
-      <img src="">  
-      <h6 class='lang'>Espana - Espanol</h6>
-      <img src="">  
-      <h6 class='lang'>United Kingdom - English</h6>
-      <img src="">  
-      <h6 class='lang'>United States - English</h6>
-      <img src="">  
-      <h6 class='lang'>Deutschland - Deutch</h6>
-      <img src="">  
-      <h6 class='lang'>France - Francias</h6>
-      <img src="">  
-      <h6 class='lang'>Nederland - Nederlands</h6>
-
+    <div class='drop-down'> 
+      <h6 class='lang'><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/7/73/Flag_of_Romania.svg/2000px-Flag_of_Romania.svg.png"><span>Romania - Romana</span></h6>   
+      <hr>
+      <h6 class='lang'><img src="https://upload.wikimedia.org/wikipedia/en/thumb/9/9a/Flag_of_Spain.svg/1280px-Flag_of_Spain.svg.png"><span>Espana - Espanol</span></h6>        
+      <hr>
+      <h6 class='lang'><img src="https://upload.wikimedia.org/wikipedia/en/thumb/a/ae/Flag_of_the_United_Kingdom.svg/1280px-Flag_of_the_United_Kingdom.svg.png"><span>United Kingdom - English</span></h6>
+      <hr>
+      <h6 class='lang'><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/1/1a/US_flag_48_stars.svg/220px-US_flag_48_stars.svg.png"><span>United States - English</span></h6>
+      <hr>
+      <h6 class='lang'><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/86/Flag_of_Germany_(3-2_aspect_ratio).svg/2000px-Flag_of_Germany_(3-2_aspect_ratio).svg.png"><span>Deutschland - Deutch</span></h6>
+      <hr>
+      <h6 class='lang'><img src="https://upload.wikimedia.org/wikipedia/en/thumb/c/c3/Flag_of_France.svg/1280px-Flag_of_France.svg.png"><span>France - Francias</span></h6>
+      <hr>
+      <h6 class='lang'><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/20/Flag_of_the_Netherlands.svg/2000px-Flag_of_the_Netherlands.svg.png"><span>Nederland - Nederlands</span></h6>
     </div>
-
   `;
 };
 
